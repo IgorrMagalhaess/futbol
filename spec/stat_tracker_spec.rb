7631161338680e33ce8a648c6c80cfc9e7ed2b5b
lance butler <lancej2004@gmail.com>
2024-01-13 12:08:54 -0700

    Tests: Teams_tackles

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -207,4 +207,10 @@ RSpec.describe StatTracker do
          expect(@stat_tracker.least_accurate_team("20122013")).to eq("Sporting Kansas City")
       end
    end
+
+   describe '#most_tackles' do
+      it "Name the Team with the most tackles in the season" do
+         expect(@stat_tracker.most_tackles("20122013")).to eq("FC Dallas")
+      end
+   end
 end
